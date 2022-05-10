6119a970c49019cfd92a1d6d0a5ba165536ebb92
AhilanAshwin <ahilanaashwin@gmail.com>
2022-05-10 15:35:35 +0800

    Added config file

---
+++ app/config.py
@@ -0,0 +1,12 @@
+from functools import lru_cache
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    project_name: str = "how-to-deploy-celery-to-aws"
+    version: str = "0.1.0"
+
+
+@lru_cache
+def get_settings():
+    return Settings()
---
+++ app/start.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from app.config import Settings, get_settings
+
+settings = get_settings()
+app = FastAPI(title=settings.project_name, version=settings.version)
+
+
+@app.get("/", status_code=200)
+def get_index():
+    return {'title': 'Hello World', 'author': "Ahilan Ashwin", 'version': "0.0.1"}
+
+
+@app.get('/ping', status_code=200)
+def healthcheck():
+    return {'status': "Success"}
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080, log_level="debug")
