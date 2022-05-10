2730dcb654150046084f4e791ac939042ddf10fc
AhilanAshwin <ahilanaashwin@gmail.com>
2022-05-10 16:17:08 +0800

    docker compose to simulate redis and rabbitmq

--- app/config.py
+++ app/config.py
@@ -1,11 +1,14 @@
 from functools import lru_cache
-from pydantic import BaseSettings
+from pydantic import BaseSettings, RedisDsn, AmqpDsn
 
 
 class Settings(BaseSettings):
     project_name: str = "how-to-deploy-celery-to-aws"
     version: str = "0.1.0"
     author: str = "Ahilan Ashwin"
+    # To be changed to sqs and elastic cache
+    redis_dsn: RedisDsn = 'redis://localhost:6379'
+    amqp_dsn: AmqpDsn = 'amqp://guest:guest@localhost:5672'
 
 
 @lru_cache
