72a5b7931d4087b9763adb7995fe6f6d5346ac52
AhilanAshwin <ahilanaashwin@gmail.com>
2022-05-16 00:25:52 +0800

    Changed celery config

--- app/celery_etl/celery.py
+++ app/celery_etl/celery.py
@@ -4,7 +4,7 @@ from app.config import get_settings
 settings = get_settings()
 celery_app = Celery(main=settings.PROJECT_NAME,
                     broker=settings.CELERY_BROKER_URL,
-                    backend=settings.CELERY_RESULT_BACKEND,
+                    backend=settings.CELERY_RESULT_BACKEND
                     include=["app.celery_etl.tasks"])
 
 if settings.ENVIRONMENT == 'prod':
